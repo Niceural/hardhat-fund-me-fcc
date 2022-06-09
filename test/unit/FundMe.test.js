425151dfccc17d26903344d191a42141e6d60346
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-06-09 18:08:51 +0100

    11:41

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,11 +1,11 @@
-const { describe, deployments, getNamedAccounts, ethers } = require("hardhat");
+const { deployments, getNamedAccounts, ethers } = require("hardhat");
 const { assert, expect } = require("chai");
 
 describe("FundMe", function () {
     let fundMe;
     let deployer;
     let mockV3Aggregator;
-    const sendValue = ethers.utils.parseEthers("1");
+    const sendValue = ethers.utils.parseEther("1");
 
     beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer;
@@ -55,6 +55,8 @@ describe("FundMe", function () {
             );
             const transactionResponse = await fundMe.withdraw();
             const transactionReceipt = await transactionResponse.wait(1);
+            const { gasUsed, effectiveGasPrice } = transactionReceipt;
+            const gasCost = gasUsed.mul(effectiveGasPrice);
 
             const endingFundMeBalance = await fundMe.provider.getBalance(
                 fundMe.address
@@ -62,11 +64,49 @@ describe("FundMe", function () {
             const endingDeplyerBalance = await fundMe.provider.getBalance(
                 deployer
             );
+            assert.equal(endingFundMeBalance, 0);
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            );
         });
-        assert.equal(endingFundMeBalance, 0);
-        assert.equal(
-            startingFundMeBalance.add(startingDeployerBalance).toString(),
-            endingDeployerBalance.add(gasCost).toString()
-        );
+        it("allows us to withdraw with multiple funders", async function () {
+            const accounts = await ethers.getSigners();
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                );
+                await fundMeConnectedContract.fund({ value: sendValue });
+            }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+            const transactionResponse = await fundMe.withdraw();
+            const transactionReceipt = await transactionResponse.wait(1);
+            const { gasUsed, effectiveGasPrice } = transactionReceipt;
+            const gasCost = gasUsed.mul(effectiveGasPrice);
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const endingDeplyerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+            assert.equal(endingFundMeBalance, 0);
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            );
+
+            await expect(fundMe.funders(0)).to.be.reverted;
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address)
+                );
+            }
+        });
     });
 });
